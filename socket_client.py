5cfc34fbbae680fc941d4e2e81a370c4764061ac
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-28 03:01:50 +0530

    M

--- socket_client.py
+++ socket_client.py
@@ -3,3 +3,7 @@ import socket
 client_socket = socket.socket()
 # IP address and port needed as args as a tuple
 client_socket.connect(('localhost' , 9999))
+
+# Printing what client received from server
+# 1024 is buffer size here
+print(client_socket.recv(1024))
