40c06bcd2b3442c711163a2ddd3e17b5051d4fd4
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-28 02:44:33 +0530

    M

--- socket.py
+++ socket.py
@@ -2,4 +2,7 @@
 # Sockets are base of internet
 # Port number and connection type , 2 imp. concepts
 # If a machine provides different services each service will have unique port number
+# Use available port number , If tomcat installed it uses 8080 port , then don't use 8080 on other services
 
+# TCP --> Connection Oriented Network (sends data only after connection)
+# UDP --> Connection Less Network (Dis Adv. Not sure if packet has reached other side)
