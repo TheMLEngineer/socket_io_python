a144d867f424f274970cf6e5bf14e3de91a03d46
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-05-28 02:41:54 +0530

    M

--- socket.py
+++ socket.py
@@ -1,2 +1,5 @@
 # In internet we have server and client
-# 
+# Sockets are base of internet
+# Port number and connection type , 2 imp. concepts
+# If a machine provides different services each service will have unique port number
+
